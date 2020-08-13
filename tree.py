f44f6b1b6e4038360db23d672cd1691cfbfe9066
james <jameshaller27@gmail.com>
2020-08-12 23:36:09 -0700

    Properly handle leaf-level arrays

--- tree.py
+++ tree.py
@@ -10,23 +10,35 @@ def data_struct_as_tree(data, indent='') -> str:
 
     if isinstance(data, dict) and len(data) > 0:
         keys = list(data.keys())
-        for kidx in range(len(keys) - 1):
+        for kidx in range(len(keys)):
             key = keys[kidx]
 
-            out_str += f'\n{indent}{SPLIT_INDENT} {key}'
-            out_str += data_struct_as_tree(data[key], indent + PIPED_INDENT)
+            if kidx < (len(keys) - 1):
+                next_indent_append = PIPED_INDENT
+                item_indent = SPLIT_INDENT
+            else:
+                next_indent_append =  EMPTY_INDENT
+                item_indent = ELBOW_INDENT
 
-        key = keys[-1]
-        out_str += f'\n{indent}{ELBOW_INDENT} {key}'
-        out_str += data_struct_as_tree(data[key], indent + EMPTY_INDENT)
+            out_str += f'\n{indent}{item_indent} {key}'
+            out_str += data_struct_as_tree(data[key], indent + next_indent_append)
 
     elif isinstance(data, list) and len(data) > 0:
-        for idx in range(len(data) - 1):
-            out_str += f'\n{indent}{SPLIT_INDENT}{ARRAY_INDEX}'
-            out_str += data_struct_as_tree(data[idx], indent + PIPED_INDENT)
+        for idx in range(len(data)):
+            child = data[idx]
 
-        out_str += f'\n{indent}{ELBOW_INDENT}{ARRAY_INDEX}'
-        out_str += data_struct_as_tree(data[-1], indent + EMPTY_INDENT)
+            if idx < (len(data) - 1):
+                next_indent_append = PIPED_INDENT
+                item_indent = SPLIT_INDENT
+            else:
+                next_indent_append =  EMPTY_INDENT
+                item_indent = ELBOW_INDENT
+
+            if isinstance(child, dict) or isinstance(child, list):
+                out_str += f'\n{indent}{item_indent}{ARRAY_INDEX}'
+                out_str += data_struct_as_tree(data[idx], indent + next_indent_append)
+            else:
+                out_str += f'\n{indent}{item_indent} {child}'
 
     else:
         out_str += f': {data}'
